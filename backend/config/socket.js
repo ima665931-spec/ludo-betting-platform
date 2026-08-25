1e0fb38f20cfd6e1e64efec066a5b4e57ac7b0e4
ima665931-spec <ima665931@gmail.com>
2026-08-25 19:15:07 +0530

    Add full betting flow: auth, home, matchmaking, wallet UI

--- backend/config/socket.js
+++ backend/config/socket.js
@@ -2,21 +2,24 @@ const socketManager = require('../socket/socketManager');
 const registerPlayerHandlers = require('../handlers/playerHandler');
 const registerRoomHandlers = require('../handlers/roomHandler');
 const registerGameHandlers = require('../handlers/gameHandler');
+const registerMatchmakingHandlers = require('../handlers/matchmakingHandler');
 const { sessionMiddleware, wrap } = require('../config/session');
 
 module.exports = function (server) {
     socketManager.initialize(server);
-    socketManager.getIO().engine.on('initial_headers', (headers, req) => {
+    const io = socketManager.getIO();
+    io.engine.on('initial_headers', (headers, req) => {
         if (req.cookieHolder) {
             headers['set-cookie'] = req.cookieHolder;
             delete req.cookieHolder;
         }
     });
-    socketManager.getIO().use(wrap(sessionMiddleware));
-    socketManager.getIO().on('connection', socket => {
+    io.use(wrap(sessionMiddleware));
+    io.on('connection', socket => {
         registerPlayerHandlers(socket);
         registerRoomHandlers(socket);
         registerGameHandlers(socket);
+        registerMatchmakingHandlers(socket, io);
         if (socket.request.session.roomId) {
             const roomId = socket.request.session.roomId.toString();
             socket.join(roomId);
